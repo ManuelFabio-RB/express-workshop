6f23aa172a0935648355d9a0d65a22a385db5345
ManuelFabio-RB <manuel.fabio.rb@gmail.com>
2024-01-30 17:43:07 -0600

    updated ternary operators & filter method

--- index.js
+++ index.js
@@ -3,46 +3,50 @@ const app = express()
 const {pokemon} = require('./pokedex.json')
 /**
  * Verbos HTTP
- * GET  
- * POST
- * PATCH
- * PUT 
- * DELETE
+ * GET --> Obtener recursos
+ * POST --> Almacenar recursos
+ * PATCH --> Modificar una parte de un recurso
+ * PUT --> Modificar un recurso
+ * DELETE -> Eliminar recursos  
  */
 
 app.get("/", (req, res, next)=>{
-    res.status(200)
-    res.send("Bienvenido al Pokedex")
+    return res.status(200).send("Bienvenido al Pokedex")
 })
 
 app.get('/pokemon', (req, res, next) => {
-    res.status(200)
-    res.send(pokemon)
+    return res.status(200).send(pokemon)
 })
 
 app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) =>{
     const id = req.params.id - 1
 
-    if(id >= 0 && id <= 150){
-        res.status(200)
-        return res.send(pokemon[id])
-    }
-    res.status(404)
-    res.send("Pokemon no encontrado")
+    return (id >= 0 && id <= 150) ?
+        res.status(200).send(pokemon[id]) :
+        res.status(404).send("Pokemon no encontrado")
     
 })
 
-app.get('/pokemon/:name', (req, res, next) =>{
+app.get('/pokemon/:name([A-Za-z]+)', (req, res, next) =>{
     const name = req.params.name
 
-    for(i = 0; i < pokemon.length; i++){
-        if(pokemon[i].name == name){
-            res.status(200)
-            return res.send(pokemon[i])
+    /*for(i = 0; i < pokemon.length; i++){
+        if(pokemon[i].name.toUpperCase() == name.toUpperCase()){
+            return res.status(200).send(pokemon[i])
         }
     }
-    res.status(404)
-    res.send("Pokemon no encontrado")
+    return res.status(404).send("Pokemon no encontrado")
+    */
+
+    const pk = pokemon.filter((p) =>{
+        return (p.name.toUpperCase() == name.toUpperCase()) ?
+            p :
+            null
+    })
+
+     return (pk.length > 0) ?
+        res.status(200).send(pk) :
+        res.status(404).send("Pokemon no encontrado")
 })
 
 app.listen(process.env.PORT||3000, () => {
